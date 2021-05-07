c1cd9db5537f8bbc96de34216ce4eeaf71733db3
Dishant Rathi <dishant16rathi@gmail.com>
2021-05-07 18:18:35 +0530

    Initial Commit

---
+++ Ecart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ecart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecart.settings')
+
+application = get_asgi_application()
