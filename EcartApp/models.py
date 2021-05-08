06d22d6fe1d78aa56518e6c5f5d1ea588d0ab8cc
Dishant Rathi <dishant16rathi@gmail.com>
2021-05-09 01:12:01 +0530

    models updated

--- EcartApp/models.py
+++ EcartApp/models.py
@@ -4,9 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    user_type_choices =((1,"Adimn"),(2,"staff"),(3,"Merchant"),(4,"Customer"))
-    user_type=models.CharField(max_length=225, choices=user_type_choices, default=1)
-    is_active=models.IntegerField(default=1)
+    user_type_choices = ((1,"Admin"),(2,"staff"),(3,"Merchant"),(4,"Customer"))
+    user_type = models.CharField(max_length=225, choices=user_type_choices, default=1)
     
 
 class AdminUser(models.Model):
@@ -14,23 +13,24 @@ class AdminUser(models.Model):
     auth_user_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
 
-class Staff(models.Model):
+class StaffUser(models.Model):
     profile_pic=models.FileField(default="")
-     auth_user_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    auth_user_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
 
-class Merchant(models.Model):
-    profile_pic=models.FileField(default="")
+class MerchantUser(models.Model):
     auth_user_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    profile_pic=models.FileField(default="")
     company_name=models.CharField(max_length=255)
-    gst_details.models.CharField(max_length=225)
+    gst_details=models.CharField(max_length=255)
     address=models.TextField()
+    is_added_by_admin=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
         
 class CustomerUser(models.Model):
-    
     auth_user_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    profile_pic=models,FileField(dafault="")
+    profile_pic=models.FileField(default="")
     created_at=models.DateTimeField(auto_now_add=True)
     
 class Categories(models.Model):
@@ -39,23 +39,23 @@ class Categories(models.Model):
     url_slug=models.CharField(max_length=255)
     thumbnail=models.FileField()
     description=models.TextField()
-    created_at=models.DateTimeField(auto_now_ass=True)
+    created_at=models.DateTimeField(auto_now_add=True)
     is_active=models.IntegerField(default=1)
 
 class SubCategories(models.Model):
     id=models.AutoField(primary_key=True)
-    Category_id=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    category_id=models.ForeignKey(Categories,on_delete=models.CASCADE)
     title=models.CharField(max_length=255)
     url_slug=models.CharField(max_length=255)
     thumbnail=models.FileField()
     description=models.TextField()
     created_at=models.DateTimeField(auto_now_ass=True)
-    is_active=models.IntegerField(default=1) 
+    is_active=models.IntegerField(default=1)
 
 class Products(models.Model):
     id=models.AutoField(primary_key=True)
     url_slug=models.CharField(max_length=255)
-    subCategories_id=models.ForeignKey(SubCategories,on_delete=models.CASCADE)
+    subcategories_id=models.ForeignKey(SubCategories,on_delete=models.CASCADE)
     product_name=models.CharField(max_length=255)
     brand=models.CharField(max_length=255)
     product_max_price=models.CharField(max_length=255)
@@ -64,13 +64,14 @@ class Products(models.Model):
     product_long_description=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     added_by_merchant=models.ForeignKey(MerchantUser,on_delete=models.CASCADE)
+    in_stock_total=models.IntegerField(default=1)
     is_active=models.IntegerField(default=1)
 
 
 class ProductDetails(models.CharField):
     id=models.AutoField(primary_key=True)
     primary_id=models.ForeignKey(Products,on_delete=models.CASCADE)
-    title=models.CharField(max_length==255)
+    title=models.CharField(max_length=255)
     title_details=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     in_stock_total=models.IntegerField(default=1)
@@ -86,12 +87,11 @@ class ProductMedia(models.Model):
     is_active=models.IntegerField(default=1)
 
 
-
 class ProductTransaction(models.Model):
     id=models.AutoField(primary_key=True)
     transaction_type_choices=((1,"BUY"),(2,"SELL"))
     product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
-    transaction_product_count=models,IntegerField(dafault=1)
+    transaction_product_count=models.IntegerField(dafault=1)
     transaction_type=models.CharField(choices=transaction_type_choices,max_length=255)
     transaction_description=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
@@ -99,13 +99,13 @@ class ProductTransaction(models.Model):
 
 class ProductAbout(models.CharField):
     id=models.AutoField(primary_key=True)
-    title=models.CharField(max_length==255)
+    title=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     is_active=models.IntegerField(default=1)
 
 class ProductTags(models.Model):
     id=models.AutoField(primary_key=True)
-    title=models.CharField(max_length==255)
+    title=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     is_active=models.IntegerField(default=1)
 
@@ -124,8 +124,8 @@ class ProductReviews(models.Model):
     id=models.AutoField(primary_key=True)
     product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
     user_id=models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
-    review_image=models,FileField()
-    rating=models.CharField(default="5")
+    review_image=models.FileField()
+    rating=models.CharField(default="5",max_length=255)
     review=models.TextField(default="")
     created_at=models.DateTimeField(auto_now_add=True)
     is_active=models.IntegerField(default=1)
